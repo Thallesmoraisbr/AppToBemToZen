ac8cfdd6d2dae7717f69e8a9ec06d07e46604fb4
Thalles <thalleslimabr@gmail.com>
2024-12-09 20:43:49 -0300

    Atividade 4 concluida

--- area_professor/static/area_professor/js/simulado.js
+++ area_professor/static/area_professor/js/simulado.js
@@ -9,52 +9,63 @@ function simuladoApp() {
     const alerta = document.querySelector("#questao_alerta");
     const table = document.querySelector("#questao_table");
     const itens = document.querySelector("#questao_itens");
-    const questoesselecionadas  = document.querySelector("#questoesselecionadas");
+    const questoesselecionadas = document.querySelector("#questoesselecionadas");
 
     questoesselecionadas.value = "";
 
     let questao_lista = document.querySelector("#questao_lista");
-    for (element of questao_lista.children) {
+    for (let element of questao_lista.children) {
         questoes_do_professor.push(element.value);
     }
 
+    function mostrarAlerta(mensagem) {
+        alerta.textContent = mensagem;
+        alerta.classList.remove("d-none");
+        alerta.classList.add("alert", "alert-danger");
+    }
+
+    function ocultarAlerta() {
+        alerta.classList.add("d-none");
+        alerta.textContent = "";
+    }
+
     btn_add.addEventListener("click", (evt) => {
-        // debugger
+        ocultarAlerta();
+
         if (pesquisa.value == "") {
             // TODO: Avisar que deve ser informado um valor no campo
+            mostrarAlerta("Por favor, informe um valor no campo de pesquisa.");
             pesquisa.value = "";
-            return
-        };
-        
+            return;
+        }
+
         if (questoes_do_professor.indexOf(pesquisa.value) < 0) {
             // TODO: Informar valor inválido
+            mostrarAlerta("Valor inválido! A questão informada não foi encontrada.");
             pesquisa.value = "";
             return;
         }
 
-        const [id, area, questao] = pesquisa.value.split(" - ")
+        const [id, area, questao] = pesquisa.value.split(" - ");
 
-        if (questoes_adicionadas.indexOf(id) >= 0){
+        if (questoes_adicionadas.indexOf(id) >= 0) {
             // TODO: Avisar que a questão já foi adicionada
+            mostrarAlerta("A questão já foi adicionada.");
             pesquisa.value = "";
             return;
         }
-        
+
         questoes_adicionadas.push(id);
-        /* <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>@mdo</td>
-        </tr> */
+
         const tr = document.createElement("tr");
         tr.innerHTML = `<th scope='row'>${id}</th><td>${area}</td><td>${questao}</td>`;
         itens.appendChild(tr);
 
-        alerta.classList.add("d-none");
+        ocultarAlerta();
         table.classList.remove("d-none");
 
         pesquisa.value = "";
-        questoesselecionadas.value = questoes_adicionadas.join(",")
+        questoesselecionadas.value = questoes_adicionadas.join(",");
     });
 }
 
