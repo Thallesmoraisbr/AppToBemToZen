1053850fc059325f911176edbafdfe6902b82d50
Thalles <thalleslimabr@gmail.com>
2024-12-09 21:19:56 -0300

    Atividade 5 tentei

--- homepage/models.py
+++ homepage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # TODO: Criar model ForumPergunta
@@ -11,3 +12,11 @@ from django.db import models
 # - pergunta ForumPergunta
 # - resposta text
 # - criado_por User
+
+class ForumPergunta(models.Model):
+    pergunta = models.TextField()
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.pergunta[:50]
